0b150618ae2e7f8d1549213e7fd3578a0f3b23c4
Toto Minai <bufhdy@hotmail.com>
2021-08-01 23:49:24 +0900

    Refactoring for Vue.js

--- utai/js/main.js
+++ utai/js/main.js
@@ -1,16 +1,28 @@
 const Desktop = {
 	data() {
 		return {
-			page3ScreenshotURLs: [
-				'screenshots/screenshot_page_3_1.png',
-				'screenshots/screenshot_page_3_2.png',
-				'screenshots/screenshot_page_3_3.png'
-			],
-			page2ScreenshotURLs: [
-				'screenshots/screenshot_page_2_1.png',
-				'screenshots/screenshot_page_2_2.png',
-				'screenshots/screenshot_page_2_4.png'
-			]
+			screenshot: {
+				page3ScreenshotURLs: [
+					'screenshots/screenshot_page_3_1.png',
+					'screenshots/screenshot_page_3_2.png',
+					'screenshots/screenshot_page_3_3.png'
+				],
+				page2ScreenshotURLs: [
+					'screenshots/screenshot_page_2_1.png',
+					'screenshots/screenshot_page_2_2.png',
+					'screenshots/screenshot_page_2_4.png'
+				]
+			},
+			download: {
+				latest: {
+					url: "https://github.com/toto-minai/Utai/releases/download/v0.1_5/Utai.app.zip",
+					description: "0.1 Build 5, requires macOS 12 Monterey Beta 3+"
+				},
+				legacy: {
+					url: "https://github.com/toto-minai/Utai/releases/download/v0.1_4/Utai.app.zip",
+					description: "Build 4"
+				}
+			}
 		}
 	}
 }
@@ -35,5 +47,21 @@ desktop.component('page-2-window', {
     </div>
 </div>
 `
+})
+desktop.component('download', {
+	props: ['latest', 'legacy'],
+	template: `
+<div class="banner_horizontal banner" style="bottom: 0;">
+    <p>
+        <a v-bind:href="latest.url">
+            Download Latest
+        </a> ({{ latest.description }}) / 
+        <a v-bind:href="legacy.url">
+            Download Legacy
+        </a> ({{ legacy.description }}) / 
+        <a href="https://github.com/toto-minai/utai">View Source</a>
+    </p>
+</div>
+	`
 })
 desktop.mount('body')
